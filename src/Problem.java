5851dac477e24864baae5a156a8f30f741fe04a5
Sina Nejati <snejati86@gmail.com>
2012-11-15 22:09:22 -0800

    Working Version

--- src/Problem.java
+++ src/Problem.java
@@ -1,4 +1,13 @@
 
 public class Problem {
 
+	public static void main (String[] args) throws CloneNotSupportedException, StateException
+	{
+		State init = new State(3,3,0,0,BoatPlacement.left);
+		State goal = new State(0,3,3,0,BoatPlacement.right);
+		Search BS = new BreadthFirstSearch();
+		BS.SetStart(init);
+		BS.SetGoal(goal);
+		BS.BeginSearch();
+	}
 }
--- src/State.java
+++ src/State.java
@@ -1,35 +1,195 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public class State {
 
-	private int _missionariesAtLeft;
-	private int _cannibalsAtLeft;
-	private int _missionariesAtRight;
-	private int _cannibalsAtRight;
-	public BoatPlacement _boat;
+public class State  {
 	
-	public State(int canLeft,int missLeft,BoatPlacement boat)
+	public int MissionariesAtLeft;
+	public int CannibalsAtLeft;
+	public int MissionariesAtRight;
+	public int CannibalsAtRight;
+	public BoatPlacement Boat;
+	public State parent;
+	public ArrayList<State> children;
+	
+	public State(int misL,int canL,int misR,int canR,BoatPlacement boat){
+		MissionariesAtLeft = misL;
+		MissionariesAtRight = misR;
+		CannibalsAtLeft = canL;
+		CannibalsAtRight = canR;
+		Boat = boat;
+	}
+	/* Copy constructor */
+	public State(State clone)
 	{
-		set_missionariesAtLeft(missLeft);
-		set_cannibalsAtLeft(canLeft);
-		_boat = boat;
+		this.MissionariesAtLeft=clone.MissionariesAtLeft;
+		this.MissionariesAtRight=clone.MissionariesAtRight;
+		this.CannibalsAtLeft=clone.CannibalsAtLeft;
+		this.CannibalsAtRight=clone.CannibalsAtRight;
+		this.Boat=clone.Boat;
+	}
+	
+	
+	public boolean Valid(){
+		return (noMinus()&&notOver()&&noLoops()&&isValid());
+	}
+	
+	private boolean isValid(){
+		boolean leftSide = true;
+		boolean rightSide = true;
+		if (this.MissionariesAtLeft>0)
+			leftSide = this.MissionariesAtLeft-this.CannibalsAtLeft>=0;
+		if (this.MissionariesAtRight>0)
+			rightSide = this.MissionariesAtRight-this.CannibalsAtRight>=0;
+			return leftSide && rightSide;
+	}
+	
+	private boolean noMinus(){
+		return (this.MissionariesAtLeft>=0 && this.MissionariesAtRight>=0 && 
+				this.CannibalsAtLeft>=0 && this.CannibalsAtRight>=0);
+	}
+	
+	private boolean notOver(){
+		return (this.MissionariesAtLeft<4 && this.MissionariesAtRight<4 && 
+				this.CannibalsAtLeft<4 && this.CannibalsAtRight<4);
+	}
+	
+	private boolean noLoops(){
+		return !((this.compare( this.parent.parent)==0));
+	}
+	
+	
+	
+	public void MoveBoat(State node){
+		if (node.Boat==BoatPlacement.left)
+		{
+			node.Boat=BoatPlacement.right;
+		}
+		else
+			node.Boat=BoatPlacement.left;
+	}
+	
+//	public void search(State node){
+//		node.ExpandChildren();
+//		for (int i = 0 ; i < node.children.size();++i)
+//		{
+//			search(node.children.get(i));
+//		}
+//	}
+	
+//	public void ExpandChildren(){
+//		children = new ArrayList<State>();
+//		for ( int i = 0 ; i < 3 ; ++i)
+//		{
+//			for ( int j = 0 ; i+j < 3 ; ++j)
+//			{
+//				if (i+j==0)
+//				{
+//					continue;
+//				}
+//				
+//				State copy = new State(this);
+//				if (this.Boat==BoatPlacement.left)
+//				{
+//				copy.CannibalsAtLeft-=j;
+//				copy.CannibalsAtRight+=j;
+//				copy.MissionariesAtLeft-=i;
+//				copy.MissionariesAtRight+=i;
+//				}
+//				else
+//				{
+//					copy.CannibalsAtLeft+=j;
+//					copy.CannibalsAtRight-=j;
+//					copy.MissionariesAtLeft+=i;
+//					copy.MissionariesAtRight-=i;	
+//				}
+//				copy.parent=this;
+//				MoveBoat(copy);
+//				if (copy.Valid())
+//				{
+//					copy.parent=this;
+//					this.children.add(copy);
+//					if (copy.Goal())
+//					{
+//						PrintPath(copy);
+//						System.exit(1);
+//						return ;
+//					}
+//				}
+//			
+//			}
+//		}
+//	}
+
+	private void PrintPath(State node) {
+		while (node.parent!=null)
+		{
+			System.out.println(node.CannibalsAtLeft+","+node.MissionariesAtLeft+","+node.Boat.toString());
+			node=node.parent;
+		}
+		System.out.println(node.CannibalsAtLeft+","+node.MissionariesAtLeft+","+node.Boat.toString());
+		System.out.println("------------------");
+		
+	}
+	
+	public void MoveCannibals(int number,BoatPlacement direction){
+		if (direction==BoatPlacement.left){
+			this.CannibalsAtLeft-=number;
+			this.CannibalsAtRight+=number;
+		}
+		else{
+			this.CannibalsAtLeft+=number;
+			this.CannibalsAtRight-=number;
+		}
+	}
+	
+	public void MoveMissionaries(int number,BoatPlacement direction){
+		if (direction==BoatPlacement.left){
+			this.MissionariesAtLeft-=number;
+			this.MissionariesAtRight+=number;
+		}
+		else{
+			this.MissionariesAtLeft+=number;
+			this.MissionariesAtRight-=number;
+		}	
+	}
+	
+	public void MoveBoat(){
+		if (this.Boat==BoatPlacement.left){
+			this.Boat=BoatPlacement.right;
+		}
+		else{
+			this.Boat=BoatPlacement.left;
+		}
+	}
+	
+	public String toString()
+	{
+		if (this.Boat==BoatPlacement.left)
+		return " Missionaries :"+this.MissionariesAtLeft+" "+"Cannibals :"+this.CannibalsAtLeft+" \\__/ "
+				+" |||||| "+" Missionaries :"+this.MissionariesAtRight+" "+"Cannibals :"+this.CannibalsAtRight;
+		else
+			return " Missionaries :"+this.MissionariesAtLeft+" "+"Cannibals :"+this.CannibalsAtLeft
+			+" |||||| "+" Missionaries :"+this.MissionariesAtRight+" "+"Cannibals :"+this.CannibalsAtRight+" \\__/ ";
 	}
 
-	public int get_missionariesAtLeft() {
-		return _missionariesAtLeft;
+	public int compare(State o2) 
+	{
+		// TODO Auto-generated method stub
+		if (this==null || o2==null)
+		{
+			return -1;
+		}
+		return equal(this,o2) ? 0 : -1;
 	}
-
-	public void set_missionariesAtLeft(int _missionariesAtLeft) {
-		this._missionariesAtLeft = _missionariesAtLeft;
-		this._missionariesAtRight = 6 - this._missionariesAtLeft;
+	
+	private boolean equal(State o1,State o2)
+	{
+		return ((o1.CannibalsAtLeft==o2.CannibalsAtLeft
+				&& o1.CannibalsAtRight==o2.CannibalsAtRight
+				&& o1.MissionariesAtLeft==o2.MissionariesAtLeft
+				&& o1.MissionariesAtRight==o2.MissionariesAtRight
+				&& o1.Boat==o2.Boat));
 	}
-
-	public int get_cannibalsAtLeft() {
-		return _cannibalsAtLeft;
-	}
-
-	public void set_cannibalsAtLeft(int _cannibalsAtLeft) {
-		this._cannibalsAtLeft = _cannibalsAtLeft;
-		this._cannibalsAtRight = 6 - this._cannibalsAtLeft;
-	}
-
+	 
 }
